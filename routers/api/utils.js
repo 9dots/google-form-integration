87f159abf15e65103df63ec88267876088ff1e4f
danleavitt0 <danleavitt0@gmail.com>
2018-07-13 16:42:30 -0700

    build version with error messages

--- routers/api/utils.js
+++ routers/api/utils.js
@@ -28,7 +28,7 @@ async function addPermission (id, access_token) {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        role: 'reader',
+        role: 'writer',
         type: 'anyone'
       })
     }

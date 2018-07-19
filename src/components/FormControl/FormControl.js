db41124a38fe490cf6570c009a40f31b20d1d93f
Michael Weintraub <mweint@gmail.com>
2018-07-19 11:00:17 -0700

    added side nav buttons

--- src/components/FormControl/FormControl.js
+++ src/components/FormControl/FormControl.js
@@ -16,44 +16,56 @@ const FormControl = props => {
     isLast
   } = props
 
-  const percent = submitted ? 100 : Math.ceil((page / fields.length) * 100)
+  // const percent = submitted ? 100 : Math.ceil((page / fields.length) * 100)
 
   return (
-    <div className='form-control-wrap'>
-      <Row
-        type='flex'
-        align='center'
-        justify='space-between'
-        className='form-control'>
-        <Col>
-          {!submitted && (
-            <span>
-              <Button
-                disabled={isFirst}
-                className='form-control-btn'
-                type='secondary'
-                onClick={back}>
-                <Icon type='left' />
-                Back
-              </Button>
-              <Button
-                className='form-control-btn'
-                type='primary'
-                onClick={isLast ? submit : next}>
-                {isLast ? 'Submit' : 'Next'}
-                <Icon type='right' />
-              </Button>
-            </span>
-          )}
-        </Col>
-        <Col style={{ minWidth: 200 }}>
-          <Progress strokeWidth={15} percent={percent} />
-          <div className='page-display'>
-            {submitted ? 'Completed' : `Page ${page + 1} of ${fields.length}`}
-          </div>
-        </Col>
-      </Row>
-    </div>
+    <span>
+      <Button disabled={isFirst} onClick={back} className='form-float-btn back'>
+        <Icon type='left' />
+      </Button>
+      <Button
+        type='primary'
+        onClick={isLast ? submit : next}
+        className={`form-float-btn next ${isLast && 'last'}`}>
+        {isLast && 'Submit '}
+        <Icon type='right' />
+      </Button>
+      <div className='form-control-wrap'>
+        <Row
+          type='flex'
+          align='center'
+          justify='space-between'
+          className='form-control'>
+          <Col>
+            {!submitted && (
+              <span>
+                <Button
+                  disabled={isFirst}
+                  className='form-control-btn'
+                  type='secondary'
+                  onClick={back}>
+                  <Icon type='left' />
+                  Back
+                </Button>
+                <Button
+                  className='form-control-btn'
+                  type='primary'
+                  onClick={isLast ? submit : next}>
+                  {isLast ? 'Submit' : 'Next'}
+                  <Icon type='right' />
+                </Button>
+              </span>
+            )}
+          </Col>
+          {/* <Col style={{ minWidth: 200 }}>
+            <Progress strokeWidth={15} percent={percent} />
+            <div className='page-display'>
+              {submitted ? 'Completed' : `Page ${page + 1} of ${fields.length}`}
+            </div>
+          </Col> */}
+        </Row>
+      </div>
+    </span>
   )
 }
 

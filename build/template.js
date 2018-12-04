9f6b127c7023234a4e27a0369dbbe57f160689c4
danleavitt0 <danleavitt0@gmail.com>
2018-12-04 10:20:34 -0800

    add sentry

--- build/template.js
+++ build/template.js
@@ -1,17 +1,19 @@
-module.exports = html => `<!doctype html>
+module.exports = html => `<!DOCTYPE html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="shortcut icon" href="/favicon.ico">
-  <title>React App</title>
-  <link href="/static/css/main.5487d647.css" rel="stylesheet">
-</head>
-
-<body>
-  <div id="root">${html}</div>
-  <script type="text/javascript" src="/static/js/main.94684da6.js"></script>
-</body>
-
-</html>`
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <script
+      src="https://browser.sentry-cdn.com/4.1.0/bundle.min.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <title>React App</title>
+    <link href="/static/css/main.5487d647.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div id="root">${html}</div>
+    <script type="text/javascript" src="/static/js/main.a2fee29c.js"></script>
+  </body>
+</html>
+`
